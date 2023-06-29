e219943dffed792a6940c04878118d75bfda81d1
Ramana Gopala Surya Gokul <peddinti.ramana-gopala-surya-gokul@capgemini.com>
2023-06-29 16:07:05 +0530

    retrieved user profile details

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import "./App.css";
 import Login from "./components/Login";
 import Player from "./components/Player.jsx";
 import { getTokenFromUrl } from "./utils/spotify";
 
+const spotify = new SpotifyWebApi();
+
 function App() {
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-    const hash = getTokenFromUrl();
+    const hash = getTokenFromUrl(); // gives obj having access_token, expires in..etc
     window.location.hash = ""; // hide the access token in url
 
     const _token = hash.access_token;
 
     if (_token) {
       setToken(_token);
+
+      console.log(spotify);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => console.log(user));
     }
   }, []);
 
